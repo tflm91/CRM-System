14f49ec5dea6c4a5770782c551c2206558a08668
Tom Frederik Leimbrock <tom.leimbrock@mni.thm.de>
2021-09-23 00:16:52 +0200

    Set routes for the customer
    This commit adds methods to add, retrieve,
    modify and delete customers.

--- index.js
+++ index.js
@@ -29,3 +29,88 @@ var router = express();
 router.listen(8080);
 router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
+router.post("/customer", function (req, res) {
+    var lastName = req.body.lastName;
+    var firstName = req.body.firstName;
+    var street = req.body.street;
+    var houseNumber = Number(req.body.houseNumber);
+    var postalCode = Number(req.body.postalCode);
+    var city = req.body.city;
+    var emailAddress = req.body.emailAddress;
+    var phoneNumber = req.body.phoneNumber;
+    if (lastName !== undefined && firstName !== undefined && street !== undefined && houseNumber !== undefined && !isNaN(houseNumber) && postalCode !== undefined && !isNaN(postalCode) && city !== undefined && emailAddress !== undefined && phoneNumber !== undefined) {
+        query("INSERT INTO Customer (lastName, firstName, street, houseNumber, postalCode, city, emailAddress, phoneNumber) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [lastName, firstName, street, houseNumber, postalCode, city, emailAddress, phoneNumber]).then(function () {
+            var id;
+            query("SELECT MAX(id) AS resId FROM Customer").then(function (results) {
+                id = Number(results[0].resId);
+                res.status(201);
+                res.send("/customer/" + id);
+            }).catch(function (reason) {
+                console.log(reason);
+                res.sendStatus(500);
+            });
+        }).catch(function (reason) {
+            console.log(reason);
+            res.sendStatus(500);
+        });
+    }
+    else {
+        res.sendStatus(400);
+    }
+});
+router.get("/customer", function (req, res) {
+    query("SELECT * FROM Customer").then(function (results) {
+        res.status(200);
+        res.json(results);
+    }).catch(function (reason) {
+        console.log("reason");
+        res.sendStatus(500);
+    });
+});
+router.put("/customer/:id", function (req, res) {
+    var id = req.params.id;
+    var lastName = req.body.lastName;
+    var firstName = req.body.firstName;
+    var street = req.body.street;
+    var houseNumber = Number(req.body.houseNumber);
+    var postalCode = Number(req.body.postalCode);
+    var city = req.body.city;
+    var emailAddress = req.body.emailAddress;
+    var phoneNumber = req.body.phoneNumber;
+    if (lastName !== undefined && firstName !== undefined && street !== undefined && houseNumber !== undefined && !isNaN(houseNumber) && postalCode !== undefined && !isNaN(postalCode) && city !== undefined && emailAddress !== undefined && phoneNumber !== undefined) {
+        query("UPDATE Customer SET lastName = ?, firstName = ?, street = ?, houseNumber = ?, postalCode = ?, city = ?, emailAddress = ?, phoneNumber = ? WHERE id = ?", [lastName, firstName, street, houseNumber, postalCode, city, emailAddress, phoneNumber, id]).then(function (results) {
+            if (results.affectedRows === 1) {
+                res.sendStatus(200);
+            }
+            else {
+                res.sendStatus(404);
+            }
+        }).catch(function (reason) {
+            console.log(reason);
+            res.sendStatus(500);
+        });
+    }
+    else {
+        res.sendStatus(400);
+    }
+});
+router.delete("/customer/:id", function (req, res) {
+    var id = req.params.id;
+    query("SELECT * FROM Customer WHERE id = ?", [id]).then(function (results) {
+        if (results.length == 1) {
+            query("DELETE FROM Customer WHERE id = ?", [id]).then(function () {
+                res.status(200);
+                res.json(results[0]);
+            }).catch(function (reason) {
+                console.log(reason);
+                res.sendStatus(400);
+            });
+        }
+        else {
+            res.sendStatus(404);
+        }
+    }).catch(function (reason) {
+        console.log(reason);
+        res.sendStatus(500);
+    });
+});
--- index.ts
+++ index.ts
@@ -29,3 +29,92 @@ const router: express.Express = express();
 router.listen(8080);
 router.use(express.urlencoded({extended: false}));
 router.use(express.json());
+
+router.post("/customer", (req: express.Request, res: express.Response) => {
+    const lastName: string = req.body.lastName;
+    const firstName: string = req.body.firstName;
+    const street: string = req.body.street;
+    const houseNumber: number = Number(req.body.houseNumber);
+    const postalCode: number = Number(req.body.postalCode);
+    const city: string = req.body.city;
+    const emailAddress: string = req.body.emailAddress;
+    const phoneNumber: string = req.body.phoneNumber;
+
+    if(lastName !== undefined && firstName !== undefined && street !== undefined && houseNumber !== undefined && !isNaN(houseNumber) && postalCode !== undefined && !isNaN(postalCode) && city !== undefined && emailAddress !== undefined && phoneNumber !== undefined) {
+        query("INSERT INTO Customer (lastName, firstName, street, houseNumber, postalCode, city, emailAddress, phoneNumber) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            [lastName, firstName, street, houseNumber, postalCode, city, emailAddress, phoneNumber]).then(() => {
+                let id: number;
+                query("SELECT MAX(id) AS resId FROM Customer").then((results: any) => {
+                    id = Number(results[0].resId);
+                    res.status(201);
+                    res.send("/customer/" + id);
+                }).catch((reason: any) => {
+                    console.log(reason);
+                    res.sendStatus(500);
+                })
+        }).catch((reason: any) => {
+            console.log(reason);
+            res.sendStatus(500);
+        })
+    } else {
+        res.sendStatus(400);
+    }
+})
+
+router.get("/customer", (req: express.Request, res: express.Response) => {
+    query("SELECT * FROM Customer").then((results: any) => {
+        res.status(200);
+        res.json(results);
+    }).catch((reason: any) => {
+        console.log("reason");
+        res.sendStatus(500);
+    })
+})
+
+router.put("/customer/:id", (req: express.Request, res: express.Response) => {
+    const id: string = req.params.id;
+    const lastName: string = req.body.lastName;
+    const firstName: string = req.body.firstName;
+    const street: string = req.body.street;
+    const houseNumber: number = Number(req.body.houseNumber);
+    const postalCode: number = Number(req.body.postalCode);
+    const city: string = req.body.city;
+    const emailAddress: string = req.body.emailAddress;
+    const phoneNumber: string = req.body.phoneNumber;
+
+    if(lastName !== undefined && firstName !== undefined && street !== undefined && houseNumber !== undefined && !isNaN(houseNumber) && postalCode !== undefined && !isNaN(postalCode) && city !== undefined && emailAddress !== undefined && phoneNumber !== undefined) {
+        query("UPDATE Customer SET lastName = ?, firstName = ?, street = ?, houseNumber = ?, postalCode = ?, city = ?, emailAddress = ?, phoneNumber = ? WHERE id = ?",
+            [lastName, firstName, street, houseNumber, postalCode, city, emailAddress, phoneNumber, id]).then((results: any) => {
+                if(results.affectedRows === 1) {
+                    res.sendStatus(200);
+                } else {
+                    res.sendStatus(404);
+                }
+        }).catch((reason: any) => {
+            console.log(reason);
+            res.sendStatus(500);
+        })
+    } else {
+        res.sendStatus(400);
+    }
+})
+
+router.delete("/customer/:id", (req: express.Request, res: express.Response) => {
+    const id: string = req.params.id;
+    query("SELECT * FROM Customer WHERE id = ?", [id]).then((results: any) => {
+        if(results.length == 1) {
+            query("DELETE FROM Customer WHERE id = ?", [id]).then(() => {
+                res.status(200);
+                res.json(results[0]);
+            }).catch((reason: any) => {
+                console.log(reason);
+                res.sendStatus(400);
+            })
+        } else {
+            res.sendStatus(404);
+        }
+    }).catch((reason: any) => {
+        console.log(reason);
+        res.sendStatus(500);
+    })
+})
